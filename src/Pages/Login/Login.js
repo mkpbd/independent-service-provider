11f884171e1564280d4b5e5be6d04ad792876630
mkpbd <mostofakamal1990@gmail.com>
2022-04-09 17:00:05 +0600

    singup form add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,32 +1,48 @@
 import React from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Card, Col, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <div className="container py-5">
+      <div className="row">
+        <Col className="sm-8 col-lg-6 offset-lg-3 offset-sm-2">
+          <Card className="text-center">
+            <Card.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      {/* Social Media  Login  */}
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          {/* Social Media  Login  */}
 
-      
+          <div className="text-center">
+            <h2>---------------- OR --------------------</h2>
+            <Button className="my-2 btn btn-lg btn-secondary">
+              Google Account
+            </Button>{" "}
+            <br></br>
+            <Button className="my-2 btn btn-lg btn-secondary">
+              GitHub Account
+            </Button>
+           
+          </div>
+        </Col>
+      </div>
     </div>
   );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
-import "./Register.css"
+import React from "react";
+import { Button, Col, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
-    return (
-        <Form>
+  return (
+    <div className="container">
+      <div className="row">
+        <Col className="col-sm-8 col-lg-6 offset-lg-3 offset-sm-2">
+          <Form>
             <Form.Group className="mb-3" controlId="nameId">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Your Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    );
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Your Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <h3 className="my-4 text-center text-secondary"><Link to='/login' className="text-decoration-none">Already account for login</Link></h3>
+        </Col>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
