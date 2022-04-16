47b7c368c9522816492899fc06a956749d91778a
mostofa kamal <mostofakamal1990@gmail.com>
2022-04-17 03:17:23 +0600

    Bootstarp card add

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -1,20 +1,23 @@
+
 import React, { useContext } from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, Button } from 'react-bootstrap';
 import { ServiceContext } from '../Service/Service';
+import './SingelService.css'
 
 const SingleService = () => {
     const services = useContext(ServiceContext);
     return (
        <>
         {services.map(service => (
-             <Col>
-             <Card className='border-0 text-center d-block'>
-               <Card.Title>{service.serviceName}</Card.Title>
+             <Col key={service.id}>
+             <Card className='border-0 text-center d-block p-3 bg-dark text-white'>
+               <Card.Title className='title-font-weight my-3'>{service.serviceName}</Card.Title>
                <Card.Img className='img-fluid h-25 w-25' src={service.serviceIcon} />
                <Card.Body>
                  <Card.Text>
                   {service.serviceSummary}
                  </Card.Text>
+                 <Button type="href">Service Charge: {service.price}$</Button>
                </Card.Body>
              </Card>
            </Col>
