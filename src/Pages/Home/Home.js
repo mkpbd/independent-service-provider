a39bcbda14f37f9cea53655f86440dc41a8553a7
mostofa kamal <mostofakamal1990@gmail.com>
2022-04-18 22:46:42 +0600

    Home page update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Service from '../../components/Service/Service';
 import Banner from './Banner/Banner';
+import Contact from '../Contact/Contact';
 
 const Home = () => {
     return (
         <>
         <Banner></Banner>
         <Service></Service>
+        <Contact></Contact>
             
         </>
     );
